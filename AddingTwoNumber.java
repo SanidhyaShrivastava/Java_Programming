bbb755e0dc1345b3ab4da48a6ee300676d6520a9
EC2 Default User <ec2-user@ip-172-31-30-101.ec2.internal>
2021-04-26 15:09:09 +0000

    a

---
+++ AddingTwoNumber.java
@@ -0,0 +1,12 @@
+/**
+ * Class Untitled
+ */
+public class AddingTwoNumber {
+	public static void main(String[] args) {
+    	int firstNumber=12;
+    	int secondNumber=45;
+    	int answer=firstNumber+secondNumber;
+    	
+    	System.out.println("Result is: " + answer);
+	}
+}
