4d4080bc53f0207223179778414a58228a413b6f
EC2 Default User <ec2-user@ip-172-31-30-101.ec2.internal>
2021-04-30 11:38:02 +0000

    a

---
+++ SwitchStatement.java
@@ -0,0 +1,30 @@
+/**
+ * Class Untitled
+ */
+public class SwitchStatement {
+	public static void main(String[] args) {
+    	
+    	char alphabet='f';
+    	
+    	switch(alphabet){
+    		case 'a':
+    			System.out.println("A");
+    			break;
+    		case 'b':
+    			System.out.println("B");
+    			break;
+    		case 'c':
+    			System.out.println("C");
+    			break;
+    		case 'd':
+    			System.out.println("D");
+    			break;
+    		case 'e':
+    			System.out.println("E");
+    			break;
+    		default:
+    			System.out.println("Not Found");
+    			break;
+    	}
+	}
+}
