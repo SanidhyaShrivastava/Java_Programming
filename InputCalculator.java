a485f79705b67b0fba5f590eaa7dce8bebd3b468
Sanidhya Shrivastava <it.sanidhya@gmail.com>
2021-05-06 21:04:00 +0530

    s

---
+++ InputCalculator.java
@@ -0,0 +1,33 @@
+
+
+import java.util.Scanner;
+
+public class InputCalculator {
+
+	public static void main(String[] args) {
+		inputThenPrintSumAndAverage();
+
+	}
+	public static void inputThenPrintSumAndAverage(){
+        Scanner input=new Scanner(System.in);
+         int sum = 0;
+        int counter = 0;
+
+        while(true) {
+
+            boolean hasNextInt = input.hasNextInt();
+
+            if(hasNextInt) {
+                int userInput = input.nextInt();
+                counter++; // how many integers the user has entered
+                sum += userInput;
+            } else {
+                System.out.println("SUM = " + sum + " AVG = " + Math.round((double) sum / counter));
+                input.close();
+                break;
+            }
+            
+        }
+    }
+
+}
