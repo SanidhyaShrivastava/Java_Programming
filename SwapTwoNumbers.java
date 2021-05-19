e1ea56c7d11476c569de9db80fcbbd00a5603c20
Sanidhya Shrivastava <it.sanidhya@gmail.com>
2021-05-19 19:27:10 +0530

    a

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class SwapTwoNumbers {
+
+	public static void main(String[] args) {
+//		-----------------------Method 1------------------------
+		
+	/*	Scanner  scanner=new Scanner(System.in);
+		System.out.println("----------Before Swapping--------------");
+		System.out.print("Enter First Number: ");
+		double num1=scanner.nextDouble();
+		System.out.print("Enter Second Number: ");
+		double num2=scanner.nextDouble();
+		scanner.close();
+		
+		double temp;
+		
+		temp=num1;
+		
+		num1=num2;
+		
+		num2=temp;
+		
+		System.out.println("----------After Swapping--------------");
+		System.out.println("First Number = "+num1);
+		System.out.println("Second Number = "+num2);
+	*/	
+		
+ 	Scanner  scanner=new Scanner(System.in);
+		System.out.println("----------Before Swapping--------------");
+		System.out.print("Enter First Number: ");
+		double num1=scanner.nextDouble();
+		System.out.print("Enter Second Number: ");
+		double num2=scanner.nextDouble();
+		scanner.close();
+		
+		num1=num1-num2;
+		num2=num1+num2;
+		num1=num2-num1;
+		
+		System.out.println("----------After Swapping--------------");
+		System.out.println("First Number = "+num1);
+		System.out.println("Second Number = "+num2);
+		
+		
+ 
+
+	}
+
+}
