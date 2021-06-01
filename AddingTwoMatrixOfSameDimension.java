eeff198101cac04776109765363eee740af7141b
Sanidhya Shrivastava <it.sanidhya@gmail.com>
2021-06-01 20:16:53 +0530

    a

---
+++ AddingTwoMatrixOfSameDimension.java
@@ -0,0 +1,66 @@
+
+import java.util.Scanner;
+
+public class AddingTwoMatrixOfSameDimension {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter no. of rows: ");
+		int rows=scanner.nextInt();
+		
+		System.out.print("Enter no. of column: ");
+		int column=scanner.nextInt();
+		
+		
+		
+		System.out.println("==========First Matrix============");
+		int[][] firstMatrix=new int[rows][column];
+		
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<column; j++) {
+				firstMatrix[i][j]=scanner.nextInt();
+			}
+		}
+		
+		
+		
+		
+		System.out.println("============Second Matrix==========");
+		int[][] secondMatrix=new int[rows][column];
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<column; j++) {
+				secondMatrix[i][j]=scanner.nextInt();
+			}
+		}
+		scanner.close();
+		
+		
+		
+//		=======================Calculation=============================
+		int[][] sum=new int[rows][column];
+		
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<column; j++) {
+				sum[i][j]=firstMatrix[i][j]+secondMatrix[i][j];
+			}
+		}
+//		=================================================================
+		
+		
+		
+		
+		System.out.println("===========Addition of Matrix========");
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<column; j++) {
+				System.out.print(sum[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+
+	}
+
+}
